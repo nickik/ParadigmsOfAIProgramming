cf96474cf8dd14985ed70ea6a5727b26d2fb5e76
nickik <nickik@gmx.ch>
2010-09-14 10:52:34 +0200

    First commit

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject ParadigmsOfAIProgramming "1.0.0-SNAPSHOT"
+  :description "PoAIProgramming in Clojure"
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                   [org.clojure/clojure-contrib "1.2.0"]]
+  :dependencies-dev [[swank-clojure "1.2.1"]
+                     [lein-difftest "1.2.2"]
+                     [clj-stacktrace "0.2.0"]])
